5bf941f1629017724ed2de3e72b1c0edc5f0a86e
Nuthara <nutharayuthmini@gmail.com>
2023-03-29 16:07:24 +0530

    Adding Text UI

--- Interface/Texts.Designer.cs
+++ Interface/Texts.Designer.cs
@@ -28,22 +28,287 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Texts));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ShapesBtn = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.TextBtn = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.TextTB = new System.Windows.Forms.RichTextBox();
+            this.bunifuElipse3 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.bunifuElipse4 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.BrailleTB = new System.Windows.Forms.RichTextBox();
+            this.bunifuElipse5 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.ConvertBtn = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.Printbtn = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 20;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.TextBtn);
+            this.panel1.Controls.Add(this.ShapesBtn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(247, 720);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(59, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Braille Printer";
+            // 
+            // ShapesBtn
+            // 
+            this.ShapesBtn.ActiveBorderThickness = 1;
+            this.ShapesBtn.ActiveCornerRadius = 20;
+            this.ShapesBtn.ActiveFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.ShapesBtn.ActiveForecolor = System.Drawing.Color.White;
+            this.ShapesBtn.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.ShapesBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.ShapesBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ShapesBtn.BackgroundImage")));
+            this.ShapesBtn.ButtonText = "Shapes";
+            this.ShapesBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ShapesBtn.Font = new System.Drawing.Font("Poppins", 14.25F);
+            this.ShapesBtn.ForeColor = System.Drawing.Color.SeaGreen;
+            this.ShapesBtn.IdleBorderThickness = 1;
+            this.ShapesBtn.IdleCornerRadius = 20;
+            this.ShapesBtn.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.ShapesBtn.IdleForecolor = System.Drawing.Color.White;
+            this.ShapesBtn.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.ShapesBtn.Location = new System.Drawing.Point(15, 267);
+            this.ShapesBtn.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.ShapesBtn.Name = "ShapesBtn";
+            this.ShapesBtn.Size = new System.Drawing.Size(214, 66);
+            this.ShapesBtn.TabIndex = 2;
+            this.ShapesBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TextBtn
+            // 
+            this.TextBtn.ActiveBorderThickness = 1;
+            this.TextBtn.ActiveCornerRadius = 20;
+            this.TextBtn.ActiveFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.TextBtn.ActiveForecolor = System.Drawing.Color.White;
+            this.TextBtn.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.TextBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.TextBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("TextBtn.BackgroundImage")));
+            this.TextBtn.ButtonText = "Texts";
+            this.TextBtn.Cursor = System.Windows.Forms.Cursors.Default;
+            this.TextBtn.Font = new System.Drawing.Font("Poppins", 14.25F);
+            this.TextBtn.ForeColor = System.Drawing.Color.SeaGreen;
+            this.TextBtn.IdleBorderThickness = 1;
+            this.TextBtn.IdleCornerRadius = 20;
+            this.TextBtn.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.TextBtn.IdleForecolor = System.Drawing.Color.White;
+            this.TextBtn.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.TextBtn.Location = new System.Drawing.Point(16, 385);
+            this.TextBtn.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.TextBtn.Name = "TextBtn";
+            this.TextBtn.Size = new System.Drawing.Size(214, 66);
+            this.TextBtn.TabIndex = 3;
+            this.TextBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 18F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(282, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(280, 42);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input Text to Convert ";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.panel2.Controls.Add(this.TextTB);
+            this.panel2.Location = new System.Drawing.Point(282, 117);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(455, 449);
+            this.panel2.TabIndex = 8;
+            // 
+            // bunifuElipse2
+            // 
+            this.bunifuElipse2.ElipseRadius = 20;
+            this.bunifuElipse2.TargetControl = this.TextTB;
+            // 
+            // TextTB
+            // 
+            this.TextTB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.TextTB.Font = new System.Drawing.Font("Poppins", 12F);
+            this.TextTB.ForeColor = System.Drawing.Color.White;
+            this.TextTB.Location = new System.Drawing.Point(3, 3);
+            this.TextTB.Name = "TextTB";
+            this.TextTB.Size = new System.Drawing.Size(450, 444);
+            this.TextTB.TabIndex = 7;
+            this.TextTB.Text = "";
+            // 
+            // bunifuElipse3
+            // 
+            this.bunifuElipse3.ElipseRadius = 20;
+            this.bunifuElipse3.TargetControl = this.panel2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.panel3.Controls.Add(this.BrailleTB);
+            this.panel3.Location = new System.Drawing.Point(795, 117);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(455, 449);
+            this.panel3.TabIndex = 10;
+            // 
+            // bunifuElipse4
+            // 
+            this.bunifuElipse4.ElipseRadius = 20;
+            this.bunifuElipse4.TargetControl = this.panel3;
+            // 
+            // BrailleTB
+            // 
+            this.BrailleTB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.BrailleTB.Font = new System.Drawing.Font("Poppins", 12F);
+            this.BrailleTB.ForeColor = System.Drawing.Color.White;
+            this.BrailleTB.Location = new System.Drawing.Point(3, 3);
+            this.BrailleTB.Name = "BrailleTB";
+            this.BrailleTB.Size = new System.Drawing.Size(450, 444);
+            this.BrailleTB.TabIndex = 7;
+            this.BrailleTB.Text = "";
+            // 
+            // bunifuElipse5
+            // 
+            this.bunifuElipse5.ElipseRadius = 20;
+            this.bunifuElipse5.TargetControl = this.BrailleTB;
+            // 
+            // ConvertBtn
+            // 
+            this.ConvertBtn.ActiveBorderThickness = 1;
+            this.ConvertBtn.ActiveCornerRadius = 20;
+            this.ConvertBtn.ActiveFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.ConvertBtn.ActiveForecolor = System.Drawing.Color.White;
+            this.ConvertBtn.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.ConvertBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(54)))), ((int)(((byte)(64)))));
+            this.ConvertBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ConvertBtn.BackgroundImage")));
+            this.ConvertBtn.ButtonText = "Convert";
+            this.ConvertBtn.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ConvertBtn.Font = new System.Drawing.Font("Poppins", 14.25F);
+            this.ConvertBtn.ForeColor = System.Drawing.Color.SeaGreen;
+            this.ConvertBtn.IdleBorderThickness = 1;
+            this.ConvertBtn.IdleCornerRadius = 20;
+            this.ConvertBtn.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.ConvertBtn.IdleForecolor = System.Drawing.Color.White;
+            this.ConvertBtn.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.ConvertBtn.Location = new System.Drawing.Point(523, 615);
+            this.ConvertBtn.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.ConvertBtn.Name = "ConvertBtn";
+            this.ConvertBtn.Size = new System.Drawing.Size(214, 66);
+            this.ConvertBtn.TabIndex = 4;
+            this.ConvertBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Printbtn
+            // 
+            this.Printbtn.ActiveBorderThickness = 1;
+            this.Printbtn.ActiveCornerRadius = 20;
+            this.Printbtn.ActiveFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.Printbtn.ActiveForecolor = System.Drawing.Color.White;
+            this.Printbtn.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.Printbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(54)))), ((int)(((byte)(64)))));
+            this.Printbtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Printbtn.BackgroundImage")));
+            this.Printbtn.ButtonText = "Print";
+            this.Printbtn.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Printbtn.Font = new System.Drawing.Font("Poppins", 14.25F);
+            this.Printbtn.ForeColor = System.Drawing.Color.SeaGreen;
+            this.Printbtn.IdleBorderThickness = 1;
+            this.Printbtn.IdleCornerRadius = 20;
+            this.Printbtn.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.Printbtn.IdleForecolor = System.Drawing.Color.White;
+            this.Printbtn.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(111)))), ((int)(((byte)(248)))));
+            this.Printbtn.Location = new System.Drawing.Point(795, 615);
+            this.Printbtn.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.Printbtn.Name = "Printbtn";
+            this.Printbtn.Size = new System.Drawing.Size(214, 66);
+            this.Printbtn.TabIndex = 12;
+            this.Printbtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Texts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(54)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.Printbtn);
+            this.Controls.Add(this.ConvertBtn);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Texts";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Texts";
+            this.Load += new System.EventHandler(this.Texts_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuThinButton2 ShapesBtn;
+        private Bunifu.Framework.UI.BunifuThinButton2 TextBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
+        private System.Windows.Forms.RichTextBox TextTB;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse3;
+        private System.Windows.Forms.Panel panel3;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse4;
+        private System.Windows.Forms.RichTextBox BrailleTB;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse5;
+        private Bunifu.Framework.UI.BunifuThinButton2 ConvertBtn;
+        private Bunifu.Framework.UI.BunifuThinButton2 Printbtn;
     }
 }
