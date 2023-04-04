b2b0fc119604b44b0e20ddb506da4ce1fd7ca667
Nawam Sahasra Mandara Denawakage <nawamsahasra27th@gmail.com>
2023-04-04 20:57:07 +0530

    Adding icon

--- Interface/Splash.Designer.cs
+++ Interface/Splash.Designer.cs
@@ -50,20 +50,18 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Poppins SemiBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(287, 70);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(215, 57);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(233, 53);
+            this.label1.Size = new System.Drawing.Size(190, 42);
             this.label1.TabIndex = 0;
             this.label1.Text = "Bralille Printer";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(333, 154);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(250, 125);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(147, 82);
+            this.pictureBox1.Size = new System.Drawing.Size(110, 67);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -73,19 +71,17 @@
             this.PercentageLbl.AutoSize = true;
             this.PercentageLbl.Font = new System.Drawing.Font("Poppins SemiBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PercentageLbl.ForeColor = System.Drawing.Color.White;
-            this.PercentageLbl.Location = new System.Drawing.Point(391, 278);
-            this.PercentageLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PercentageLbl.Location = new System.Drawing.Point(293, 226);
             this.PercentageLbl.Name = "PercentageLbl";
-            this.PercentageLbl.Size = new System.Drawing.Size(48, 53);
+            this.PercentageLbl.Size = new System.Drawing.Size(36, 42);
             this.PercentageLbl.TabIndex = 2;
             this.PercentageLbl.Text = "%";
             // 
             // MyProgress
             // 
-            this.MyProgress.Location = new System.Drawing.Point(160, 390);
-            this.MyProgress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MyProgress.Location = new System.Drawing.Point(120, 317);
             this.MyProgress.Name = "MyProgress";
-            this.MyProgress.Size = new System.Drawing.Size(503, 28);
+            this.MyProgress.Size = new System.Drawing.Size(377, 23);
             this.MyProgress.TabIndex = 3;
             // 
             // bunifuElipse2
@@ -99,16 +95,16 @@
             // 
             // Splash
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
-            this.ClientSize = new System.Drawing.Size(840, 494);
+            this.ClientSize = new System.Drawing.Size(630, 401);
             this.Controls.Add(this.MyProgress);
             this.Controls.Add(this.PercentageLbl);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Splash";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Splash";
--- Interface/Texts.Designer.cs
+++ Interface/Texts.Designer.cs
@@ -290,6 +290,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Texts";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Texts";
