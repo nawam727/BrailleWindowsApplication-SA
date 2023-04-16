7abc75295d3c9396b384da9d77396dcf39083347
KavindiViranga <97431820+KavindiViranga@users.noreply.github.com>
2023-04-16 11:33:11 +0530

    connect api with text tab

--- Interface/Texts.cs
+++ Interface/Texts.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,8 @@ namespace BrailleWindowsApplication_SA.Interface
         public Texts()
         {
             InitializeComponent();
+            TextTB.Validating += TextTBValidating;
+            ConvertBtn.Click += ConvertBtn_Click;
         }
 
         private void Texts_Load(object sender, EventArgs e)
@@ -34,6 +37,7 @@ namespace BrailleWindowsApplication_SA.Interface
             this.Hide();
         }
 
+        //To close the application
         private void ClosePic_Click(object sender, EventArgs e)
         {
             Application.Exit();
@@ -44,6 +48,7 @@ namespace BrailleWindowsApplication_SA.Interface
         
         private void TextTBValidating(object sender, CancelEventArgs e)
         {
+            
             if (string.IsNullOrWhiteSpace(TextTB.Text))
             {
                 e.Cancel = true;
@@ -57,6 +62,7 @@ namespace BrailleWindowsApplication_SA.Interface
         }
         private void ConvertBtn_Click(object sender, EventArgs e)
         {
+            PrintBraille();
             if (TextTB.Text.Trim().Length == 0)
             {
                 label3.Text = "Please enter a value.";
@@ -70,6 +76,38 @@ namespace BrailleWindowsApplication_SA.Interface
             }
         }
 
+
+        //Print braille charactors in BrailleTB
+        private void PrintBraille()
+        {
+            string text = TextTB.Text;
+            string url = $"http://localhost:8082/DotPrint/api/brailletext/{text}";
+            GetApi(url);
+        }
+
+        //Get Api data from api
+        private async void GetApi(string url)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(url);
+                string dotPrint = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    BrailleTB.Text = dotPrint;
+                }
+                else
+                {
+                    BrailleTB.Text = "Server Error";
+                }
+            }
+            catch (Exception ex)
+            {
+                BrailleTB.Text = ex.Message;
+            }
+
+        }
         private void BraillePrint_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
             string text = BrailleTB.Text;
